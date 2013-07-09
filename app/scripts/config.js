c88863bf8af3942ac24dbc0ffb7000e75dd944b2
Kalle Leinonen <kleinone@systemsbiology.org>
2013-07-08 18:23:45 -0700

    Moving to new build system. Removed unneeded files. Updated views and models to load using RequireJS.

---
+++ app/scripts/config.js
@@ -0,0 +1,58 @@
+require.config({
+    baseUrl: 'scripts',
+
+    paths: {
+        jquery: '../bower_components/jquery/jquery',
+        "jQuery-ui": "../bower_components/jquery-ui/ui/jquery-ui",
+        backbone: '../bower_components/backbone/backbone',
+        bootstrap: 'vendor/bootstrap',
+        d3: '../bower_components/d3/d3',
+        modernizr: '../bower_components/modernizr',
+        underscore: '../bower_components/underscore/underscore',
+        hbs : '../bower_components/require-handlebars-plugin/hbs',
+        handlebars : '../bower_components/require-handlebars-plugin/Handlebars',
+        'json2' : '../bower_components/require-handlebars-plugin/hbs/json2',
+        'i18nprecompile' : '../bower_components/require-handlebars-plugin/hbs/i18nprecompile',
+        vq : '../bower_components/visquick/vq'
+    },
+    shim: {
+        underscore : {
+            'exports' : '_'
+        },
+        "jQuery-ui" : {
+            "deps": ["jquery"],
+            "exports" : "$"
+        },
+        d3 : {
+            'exports' : 'd3'
+        },
+        vq : {
+            'deps' : ['d3','underscore'],
+            'exports' : 'vq'
+        },
+        backbone : {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        bootstrap : {
+            deps : ['jquery','jQuery-ui'],
+            exports : 'bootstrap'
+        }
+    },
+    hbs : {
+        "templateExtension" : 'hbs',
+        "disableI18n" : true,
+        "helperPathCallback" : // Callback to determine the path to look for helpers
+            function (name) { // ('/template/helpers/'+name by default)
+                return 'templates/helpers/' + name;
+            }
+    }
+});
+
+require(['qed', 'router'], function (QED, router) {
+    // 'use strict';
+
+    qed = QED;
+    var r = new router();
+    r.initialize();
+});
